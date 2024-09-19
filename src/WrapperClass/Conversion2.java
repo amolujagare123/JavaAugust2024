f0e9530a845dfdeca97df3a764c54025c1748bdc
amolu <amolujagare@gmail.com>
2024-09-19 09:02:02 +0530

    java

---
+++ src/WrapperClass/Conversion2.java
@@ -0,0 +1,15 @@
+package WrapperClass;
+
+public class Conversion2 {
+
+    public static void main(String[] args) {
+
+        int x = 70;
+
+        Integer i = x;
+
+        String s = i.toString();
+
+        System.out.println(s.charAt(0));
+    }
+}
